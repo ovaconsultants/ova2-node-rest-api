20b8cf321fb22c6f000ce2033386efe9a37ef73b
ovaconsultants <hr@ova2consultancy.com>
2025-01-19 16:22:33 -0500

    Added cors

--- app.js
+++ app.js
@@ -12,14 +12,14 @@ const jobApplicantRoutes = require('./routes/jobApplicantRoutes');
 
 // Remove duplicate dotenv.config() call here, as it was already done in the previous line.
 
-  const app = express();
-  app.use(cookieParser());
+const app = express();
+app.use(cookieParser());
 
-  app.use(cors({
-    origin: '*', // Use the WEB_URL from environment or fallback to localhost
-    methods: 'GET,POST',
-    // credentials: true, // Allow cookies (including JWT cookie) to be sent
-  }));
+app.use(cors({
+  origin: process.env.WEB_URL, // Use the WEB_URL from environment or fallback to localhost
+  methods: 'GET,POST',
+  //credentials: true, // Allow cookies (including JWT cookie) to be sent
+}));
 
 app.use(express.json()); // Parse JSON bodies
 
